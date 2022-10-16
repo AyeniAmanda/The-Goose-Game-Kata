92745c0102043aef3557dadc8c7e28c507a1af43
AYENI AMANDA <amandaayeni32@gmail.com>
2022-10-16 15:42:52 +0100

    unit tests for some methods

--- src/test/java/org/example/AppTest.java
+++ src/test/java/org/example/AppTest.java
@@ -1,20 +1,40 @@
 package org.example;
 
-import static org.junit.Assert.assertTrue;
+import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
 
-import org.junit.Test;
+import java.util.Arrays;
 
-/**
- * Unit test for simple App.
- */
-public class AppTest 
-{
-    /**
-     * Rigorous Test :-)
-     */
+
+public class AppTest {
+    @BeforeEach
+    void setUp() {
+        App.addPlayer("peter");
+        App.addPlayer("chioma");
+    }
+
     @Test
-    public void shouldAnswerWithTrue()
-    {
-        assertTrue( true );
+    void addPlayer() {
+
+        int expectedSize = 2;
+        int actualSize = App.players.size();
+
+        String expectedFirstPlayer = "peter";
+        String actualFirstPlayer = App.players.get(0);
+
+
+        assertEquals(expectedSize, actualSize);
+        assertEquals(expectedFirstPlayer, actualFirstPlayer);
+    }
+
+    @Test
+    void bridge() {
+        App.diceRoll = Arrays.asList(3, 3);
+        App.bridge("chioma");
+        int expected = 12;
+        int actual = App.positions.get("chioma");
+
+        assertEquals(expected, actual);
     }
 }
